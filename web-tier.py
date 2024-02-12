c75f936b26f0305b84fa320f89ffc1f6f4a888e5
Naveena Raipole <144768728+naveenaraipole97@users.noreply.github.com>
2024-02-11 17:57:20 -0700

    Add files via upload

---
+++ web-tier.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import csv
+import os
+
+app=Flask(__name__)
+
+csv_file_path='Classification_Results.csv'
+
+def getNameFromFile(csv_file,target_file_name):
+    with open(csv_file,'r') as file:
+        reader=csv.DictReader(file)
+        for row in reader:
+            if row['Image']==target_file_name:
+                # print(row['Results'])
+                return row['Results']
+
+@app.route('/',methods=['POST'])
+def post_data():
+    if 'inputFile' not in request.files:
+        return 'No File Uploaded, please upload!'
+
+    file=request.files['inputFile']
+    fileName, extension=os.path.splitext(file.filename)
+    # check if the extension is valid?
+    result=getNameFromFile(csv_file_path,fileName)
+    response_data=fileName+':'+result
+    response_data=response_data.replace('"','')
+    response_data=response_data.strip()
+    print(response_data)
+    # {'name': result,'status':'Success'}
+
+    return response_data
+
+
+if __name__=='__main__':
+   app.run(host='0.0.0.0', port=180)
